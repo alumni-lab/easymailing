dc0598d6359a18e6bb9ebbaba130ffd44d89d5e0
Tony Kieling <tony.kieling@gmail.com>
2020-01-26 15:07:45 -0800

    first server set up

---
+++ send_email.js
@@ -0,0 +1,11 @@
+/**
+ * send email function
+ */
+const send_email = (req, res) => {
+
+};
+
+
+module.exports = {
+  send_email
+};
---
+++ server.js
@@ -0,0 +1,37 @@
+const express   = require("express");
+const PORT      = process.env.PORT || 3333;
+const app       = express();
+
+// able to have a .env file to store sensible info without being public
+require('dotenv').config();
+
+// package to handle data from body
+const bodyParser  = require("body-parser");
+// packge being used
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// checks if JSON malformatted messages
+app.use((err, req, res, next) => {
+  if (err) {
+    res.status(409).json({
+      error: err.message
+    });
+  }
+  else
+    next()
+});
+
+
+// the route to get email data from front-end
+router.post("/send_email", (req, res) => {
+
+});
+
+// front-end being public and served
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public', 'index.html'))
+});
+
+// server running
+app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
+
